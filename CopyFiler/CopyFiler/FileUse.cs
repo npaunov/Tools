38db6913efc46068bb0910f6a0068a1aa767abcb
npaunov <nvpaunov@gmail.com>
2017-02-08 10:21:33 +0200

    Test Uncoment

--- CopyFiler/CopyFiler/FileUse.cs
+++ CopyFiler/CopyFiler/FileUse.cs
@@ -8,18 +8,18 @@ namespace CopyFiler
         {
             if (System.IO.Directory.Exists(sourcePath))
             {
-                //System.IO.Directory.CreateDirectory(targetPath);
+                System.IO.Directory.CreateDirectory(targetPath);
 
-                //string[] files = System.IO.Directory.GetFiles(sourcePath);
+                string[] files = System.IO.Directory.GetFiles(sourcePath);
 
-                //// Copy the files and overwrite destination files if they already exist.
-                //foreach (string s in files)
-                //{
-                //    // Use static Path methods to extract only the file name from the path.
-                //    string fileName = System.IO.Path.GetFileName(s);
-                //    string destFile = System.IO.Path.Combine(targetPath, fileName);
-                //    System.IO.File.Copy(s, destFile, true);
-                //}
+                // Copy the files and overwrite destination files if they already exist.
+                foreach (string s in files)
+                {
+                    // Use static Path methods to extract only the file name from the path.
+                    string fileName = System.IO.Path.GetFileName(s);
+                    string destFile = System.IO.Path.Combine(targetPath, fileName);
+                    System.IO.File.Copy(s, destFile, true);
+                }
 
                 CopyDir(sourcePath, targetPath);
                 return;
